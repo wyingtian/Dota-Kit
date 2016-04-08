e657e0a324aa826687f1ee1398a103272af4c577
ying <wyingtian@gmail.com>
2016-04-08 18:38:51 -0400

    add sample user data

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
          $this->call(HeroesTableSeeder::class);
-       $this->call(UserTableSeeder::class);
+         $this->call(UserTableSeeder::class);
 
     }
 }
--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use Carbon\Carbon;
 class UserTableSeeder extends Seeder
 {
     /**
@@ -11,11 +11,19 @@ class UserTableSeeder extends Seeder
      */
     public function run()
     {
-
-        $jill = new Dota2\User(array("name" => "Jill","password"=> "helloworld","email=>jill@harvard.edu","created_at"=>Carbon::now()->toDateTimeString(),"updated_at"=>Carbon::now()->toDateTimeString()));
+        $jill = new Dota2\User;
+        $jill->name = "name" ;
+        $jill->password = "helloworld";
+        $jill->email="jill@harvard.edu";
+        //created_at"=>Carbon::now()->toDateTimeString(),"updated_at"=>Carbon::now()->toDateTimeString()))
         $jill->save();
 
-        $jamal = new Dota2\User(array("name" => "jamal","password"=> "helloworld","email=>jamal@harvard.edu","created_at"=>Carbon::now()->toDateTimeString(),"updated_at"=>Carbon::now()->toDateTimeString()));
+     //   $jamal = new Dota2\User(array("name" => "jamal","password"=> "helloworld","email=>jamal@harvard.edu","created_at"=>Carbon::now()->toDateTimeString(),"updated_at"=>Carbon::now()->toDateTimeString()));
+
+        $jamal = new Dota2\User;
+        $jamal->name = "name" ;
+        $jamal->password = "helloworld";
+        $jamal->email="jamal@harvard.edu";
         $jamal->save();
     }
 }
